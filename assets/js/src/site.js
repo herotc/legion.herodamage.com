14ff41dad9120e1d615af0cc069fd04e1dcc3302
Florian Meißner <developer@mystler.eu>
2017-12-05 16:07:35 +0100

    [Relics] Update crucible weight display - Add copy to clipboard button - Move it before google chart - Remove old theme switch js stuff

--- assets/js/src/site.js
+++ assets/js/src/site.js
@@ -1,45 +1,9 @@
-function isDarkTheme() {
-  if (typeof(Storage) !== "undefined") {
-    if (localStorage.getItem("darkTheme") === "false")
-      return false;
-    else
-      return true;
-  } else {
-    // Your browser is old shit...
-    return false;
-  }
-}
-
-function setDarkTheme(enabled) {
-  if (enabled) {
-    document.body.className = "dark-theme";
-    if (typeof(Storage) !== "undefined") {
-      localStorage.setItem("darkTheme", "true");
-    }
-  } else {
-    document.body.className = "";
-    if (typeof(Storage) !== "undefined") {
-      localStorage.setItem("darkTheme", "false");
-    }
-  }
-}
-
-function updateThemeSwitchText() {
-  if (isDarkTheme())
-    document.getElementById("theme-switch").innerHTML = "Switch to Light Theme";
-  else
-    document.getElementById("theme-switch").innerHTML = "Switch to Dark Theme";
-}
-
-function onThemeSwitchClick() {
-  setDarkTheme(!isDarkTheme());
-  window.location.reload();
-}
-
-function refreshTheme() {
-  setDarkTheme(isDarkTheme());
-}
-
 function formatNumber(number) {
   return new Intl.NumberFormat("en-US").format(number);
+}
+
+function copyToClipboard(elementId) {
+  var copyText = document.getElementById(elementId);
+  copyText.select();
+  document.execCommand("Copy");
 }
