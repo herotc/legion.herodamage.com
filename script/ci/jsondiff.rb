77adc303a032c690c10bcef8ead7142d9a05c2a5
Aethys256 <aethys256@users.noreply.github.com>
2017-12-28 16:23:53 +0100

    [CI] Purge more urls equivalent

--- script/ci/jsondiff.rb
+++ script/ci/jsondiff.rb
@@ -7,7 +7,21 @@ baseUrl = Config['url']
 urlsToPurge = { }
 urls = []
 File.readlines('deploy/filenames.diff').each do |line|
-  urls.push("#{baseUrl}/#{line.chomp}")
+  relativeUrl = line.chomp
+  # Complete filename version, i.e.: https://www.herodamage.com/blog/index.html
+  urls.push("#{baseUrl}/#{relativeUrl}")
+  # Extensionless filename version for html files, i.e.: https://www.herodamage.com/blog/index
+  if relativeUrl.include? '.html'
+    urls.push("#{baseUrl}/#{relativeUrl.gsub(".html", '')}")
+  end
+  # Indexless version, i.e.: https://www.herodamage.com/blog/
+  if relativeUrl.include? 'index.html'
+    urls.push("#{baseUrl}/#{relativeUrl.gsub("index.html", '')}")
+  end
+  # Indexless version without trailing slash, i.e.: https://www.herodamage.com/blog
+  if relativeUrl.include? '/index.html'
+    urls.push("#{baseUrl}/#{relativeUrl.gsub("/index.html", '')}")
+  end
 end
 urlsToPurge['files'] = urls
 
