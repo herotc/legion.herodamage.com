de261d79547b7c910f3129de21853b2fa5404d4a
Aethys256 <aethys256@users.noreply.github.com>
2018-02-17 10:15:40 +0100

    Add _config_en_only.yml to only build en language

--- devserve.bat
+++ devserve.bat
@@ -1,5 +1,5 @@
 rd /s /q _site REM Delete _site folder
 call bundle install
 call bundle exec ruby script/generate/views.rb
-call bundle exec jekyll serve --config=_config.yml,_config_collections.yml,_config_defaults.yml,_config_dev.yml --strict_front_matter
+call bundle exec jekyll serve --config=_config.yml,_config_collections.yml,_config_defaults.yml,_config_dev.yml,_config_en_only.yml --strict_front_matter
 pause
