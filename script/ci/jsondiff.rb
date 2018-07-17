9ccbbe57321d42bdc3e82db80ba5e1548899c687
Quentin Giraud <dev@aethys.io>
2018-07-17 05:55:51 +0200

    Switch over legion.herodamage.com

--- script/ci/jsondiff.rb
+++ script/ci/jsondiff.rb
@@ -16,17 +16,17 @@ File.readlines('deploy/filenames.diff').each do |line|
   end
 
   relativeUrl = line.chomp
-  # Complete filename version, i.e.: https://www.herodamage.com/blog/index.html
+  # Complete filename version, i.e.: https://legion.herodamage.com/blog/index.html
   urls.push("#{baseUrl}/#{relativeUrl}")
-  # Extensionless filename version for html files, i.e.: https://www.herodamage.com/blog/index
+  # Extensionless filename version for html files, i.e.: https://legion.herodamage.com/blog/index
   if relativeUrl.include? '.html'
     urls.push("#{baseUrl}/#{relativeUrl.gsub(".html", '')}")
   end
-  # Indexless version, i.e.: https://www.herodamage.com/blog/
+  # Indexless version, i.e.: https://legion.herodamage.com/blog/
   if relativeUrl.include? 'index.html'
     urls.push("#{baseUrl}/#{relativeUrl.gsub("index.html", '')}")
   end
-  # Indexless version without trailing slash, i.e.: https://www.herodamage.com/blog
+  # Indexless version without trailing slash, i.e.: https://legion.herodamage.com/blog
   if relativeUrl.include? '/index.html'
     urls.push("#{baseUrl}/#{relativeUrl.gsub("/index.html", '')}")
   end
